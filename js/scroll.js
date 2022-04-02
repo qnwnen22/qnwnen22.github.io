71a09b17170693335f989dffddd618bfce2d97aa
Moka <57924805+qnwnen22@users.noreply.github.com>
2022-04-02 16:40:40 +0900

    프로필 사이트 변경

--- js/scroll.js
+++
@@ -1,5 +0,0 @@
-// function scroll_move(_id) {
-//     let location = document.querySelector(_id).offsetTop;
-//     location -= 70;
-//     window.scrollTo({ top: location, behavior: 'smooth' });
-// }
